8b9a0301c76e38ac848fff71b98db7290d2a3c62
intwocave <intwocave@gmail.com>
2024-07-06 14:25:39 +0900

    Touchpad config

--- thecave-LT_HyprArch/script/touchpad_toggle_hyprland.sh
+++ thecave-LT_HyprArch/script/touchpad_toggle_hyprland.sh
@@ -2,9 +2,9 @@
 current_setting="$(ps -e | grep evtest | tr -s " " | cut -d' ' -f5 | tail -n 1)"
 if [ "$current_setting" = "evtest" ]; then
 	killall -s 9 evtest &
-	echo -e "Touchpad is now enabled..\007";
+	echo -e "Touchpad is now enabled..";
 
 else
-	evtest --grab /dev/input/event9 > /dev/null 2>&1 &
-	echo -e "Touchpad is now disabled..\007";
+	evtest --grab `libinput list-devices | grep Touchpad -A 1 | tail -n1 | tr -s " " | cut -d' ' -f2` > /dev/null 2>&1 &
+	echo -e "Touchpad is now disabled..";
 fi
