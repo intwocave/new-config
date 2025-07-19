e0ff819b33954e8f968089bb4d1893571b700457
intwocave <intwocave@gmail.com>
2025-07-19 21:30:53 +0900

    new configs for thecave-LT_Mac

---
+++ thecave-LT_Mac/Users/apple/.hammerspoon/init.lua
@@ -0,0 +1,25 @@
+-- 원하는 키에 바인딩 (예: f18)
+hs.hotkey.bind({}, "f18", function()
+    local current = hs.keycodes.currentSourceID()
+
+    if current == "com.apple.inputmethod.Korean.2SetKorean" or current == "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese" then
+        -- 한국어 or 일본어일 때 -> 영어
+        hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
+    elseif current == "com.apple.keylayout.ABC" then
+        -- 영어일 때 -> 한국어
+        hs.keycodes.currentSourceID("com.apple.inputmethod.Korean.2SetKorean")
+    end
+end)
+
+-- 원하는 키에 바인딩 (예: cmd-f18)
+hs.hotkey.bind({"cmd"}, "f18", function()
+    local current = hs.keycodes.currentSourceID()
+
+    if current == "com.apple.keylayout.ABC" or current == "com.apple.inputmethod.Korean.2SetKorean" then
+        -- 영어 or 한국어일 때 -> 일본어
+        hs.keycodes.currentSourceID("com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese")
+    elseif current == "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese" then
+        -- 일본어일 때 -> 한국어
+        hs.keycodes.currentSourceID("com.apple.inputmethod.Korean.2SetKorean")
+    end
+end)
