d9adaeb66c6a6dfd00611c0f13e6a8e02861f02b
intwocave <intwocave@gmail.com>
2025-08-26 14:19:57 +0900

    feat: substitute hammerspoon for Shortcuts

--- thecave-LT_Mac/Users/intwocave/.hammerspoon/init.lua
+++ thecave-LT_Mac/Users/intwocave/.hammerspoon/init.lua
@@ -23,3 +23,49 @@ hs.hotkey.bind({"cmd"}, "f18", function()
         hs.keycodes.currentSourceID("com.apple.inputmethod.Korean.2SetKorean")
     end
 end)
+
+
+-- Application shortcuts --
+
+
+-- Command + Control + T로 iTerm2 단축 실행
+hs.hotkey.bind({"cmd", "ctrl"}, "T", function()
+	-- 새 창에서 새로 열기
+	hs.task.new("/usr/bin/open", nil, {"-na", "iTerm"}):start()
+end)
+
+-- Control + Command + F로 Finder 단축 실행
+hs.hotkey.bind({"cmd", "ctrl"}, "F", function()
+	-- Finder 실행 (없으면 실행, 있으면 활성화)
+	hs.application.launchOrFocus("Finder")
+end)
+
+-- Control + Shift + C로 Firefox 단축 실행
+hs.hotkey.bind({"ctrl", "shift"}, "C", function()
+	-- 새 창에서 새로 열기
+	hs.task.new("/usr/bin/open", nil, {"-na", "Firefox"}):start()
+end)
+
+-- Command + Control + E로 Notion 단축 실행
+hs.hotkey.bind({"cmd", "ctrl"}, "E", function()
+	-- Notion 실행 (없으면 실행, 있으면 활성화)
+	hs.application.launchOrFocus("Notion")
+end)
+
+-- Control + Shift + D로 Discord 단축 실행
+hs.hotkey.bind({"ctrl", "shift"}, "D", function()
+	-- Discord 실행 (없으면 실행, 있으면 활성화)
+	hs.application.launchOrFocus("Discord")
+end)
+
+-- Command + Control + Shift + D로 Discord MI 단축 실행
+hs.hotkey.bind({"cmd", "ctrl", "shift"}, "D", function()
+	-- 새 창에서 새로 열기
+	hs.task.new("/usr/bin/open", nil, {"-na", "Discord", "--args", "--multi-instance"}):start()
+end)
+
+-- Command + Control + Shift + E로 Mail 단축 실행
+hs.hotkey.bind({"cmd", "ctrl", "shift"}, "E", function()
+	-- Mail 실행 (없으면 실행, 있으면 활성화)
+	hs.application.launchOrFocus("Mail")
+end)
