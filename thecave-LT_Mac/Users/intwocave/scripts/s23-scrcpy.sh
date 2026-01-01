7da4bb3019ac36fbd8cfe1b196dcf7c9e6a0c655
intwocave <intwocave@gmail.com>
2026-01-02 00:26:31 +0900

    update script contents

---
+++ thecave-LT_Mac/Users/intwocave/scripts/s23-scrcpy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/Users/intwocave/git/scrcpy/scrcpy-macos-aarch64-v3.3.2/adb connect 192.168.35.219 && sleep 1 && /Users/intwocave/git/scrcpy/scrcpy-macos-aarch64-v3.3.2/scrcpy --turn-screen-off --stay-awake
