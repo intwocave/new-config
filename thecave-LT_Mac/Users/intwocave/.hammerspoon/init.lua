fadab407018fb5da04daead9a2394b652f352671
intwocave <intwocave@gmail.com>
2026-01-19 17:17:01 +0900

    update script

--- thecave-LT_Mac/Users/intwocave/.hammerspoon/init.lua
+++ thecave-LT_Mac/Users/intwocave/.hammerspoon/init.lua
@@ -1,13 +1,31 @@
+-- 입력 소스 ID
+local kEnglish = "com.apple.keylayout.ABC"
+local kKorean = "com.apple.inputmethod.Korean.2SetKorean"
+local kJapanese = "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese"
+
 -- f18 키에 바인딩
 hs.hotkey.bind({}, "f18", function()
     local current = hs.keycodes.currentSourceID()
 
-    if current == "com.apple.inputmethod.Korean.2SetKorean" or current == "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese" then
-        -- 한국어 or 일본어일 때 -> 영어
-        hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
-    elseif current == "com.apple.keylayout.ABC" then
-        -- 영어일 때 -> 한국어
-        hs.keycodes.currentSourceID("com.apple.inputmethod.Korean.2SetKorean")
+		-- 한국어 or 일본어일 때 -> 영어
+    if current == kKorean or current == kJapanese then
+        hs.keycodes.currentSourceID(kEnglish)
+
+				hs.timer.doAfter(0.05, function()
+						if hs.keycodes.currentSourceID() ~= kEnglish then
+								hs.keycodes.currentSourceID(kEnglish)
+						end
+				end)
+				
+		-- 영어일 때 -> 한국어
+    elseif current == kEnglish then
+        hs.keycodes.currentSourceID(kKorean)
+
+				hs.timer.doAfter(0.05, function()
+						if hs.keycodes.currentSourceID() ~= kKorean then
+								hs.keycodes.currentSourceID(kKorean)
+						end
+				end)
     end
 end)
 
@@ -15,12 +33,25 @@ end)
 hs.hotkey.bind({"cmd"}, "f18", function()
     local current = hs.keycodes.currentSourceID()
 
-    if current == "com.apple.keylayout.ABC" or current == "com.apple.inputmethod.Korean.2SetKorean" then
-        -- 영어 or 한국어일 때 -> 일본어
-        hs.keycodes.currentSourceID("com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese")
-    elseif current == "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese" then
-        -- 일본어일 때 -> 한국어
-        hs.keycodes.currentSourceID("com.apple.inputmethod.Korean.2SetKorean")
+		-- 영어 or 한국어일 때 -> 일본어
+    if current == kEnglish or current == kKorean then
+        hs.keycodes.currentSourceID(kJapanese)
+
+				hs.timer.doAfter(0.05, function()
+						if hs.keycodes.currentSourceID() ~= kJapanese then
+								hs.keycodes.currentSourceID(kJapanese)
+						end
+				end)
+				
+		-- 일본어일 때 -> 한국어
+    elseif current == kJapanese then
+        hs.keycodes.currentSourceID(kKorean)
+
+				hs.timer.doAfter(0.05, function()
+						if hs.keycodes.currentSourceID() ~= kKorean then
+								hs.keycodes.currentSourceID(kKorean)
+						end
+				end)
     end
 end)
 
