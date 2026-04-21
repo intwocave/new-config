bc8f8c64d70ce520cd78ce8a373638a69e0b5513
hamisien <soboruyu@gmail.com>
2026-04-22 00:13:52 +0900

    iterm2 shell integration

--- gensocom-dt/home/hms/scripts/startComfyServer.sh
+++ gensocom-dt/home/hms/scripts/startComfyServer.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-source ~/git/ComfyUI/.venv/bin/activate; python ~/git/ComfyUI/main.py --enable-manager --listen 0.0.0.0; deactivate
+# --enable-manager, --disable-all-custom-nodes
+source ~/git/ComfyUI/.venv/bin/activate; python ~/git/ComfyUI/main.py --disable-all-custom-nodes --listen 0.0.0.0; deactivate
