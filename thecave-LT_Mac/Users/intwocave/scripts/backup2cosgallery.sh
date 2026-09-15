4c44f3507de9f80114188851ee16133b1ee4cb82
intwocave <intwocave@gmail.com>
2026-09-15 14:20:58 +0900

    feat: update backup2cosgallery.sh

--- thecave-LT_Mac/Users/intwocave/scripts/backup2cosgallery.sh
+++ thecave-LT_Mac/Users/intwocave/scripts/backup2cosgallery.sh
@@ -6,18 +6,19 @@ echo -n "-> "
 read value
 
 COMMON_OPTS=('--modify-window=1' '--exclude=._*' '--exclude=.fseventsd' '--exclude=.Spotlight-V100' '--exclude=.Trashes' '--exclude=.DS_Store' '--exclude=*~' '--delete' '--delete-excluded' '--iconv=utf-8-mac,utf-8')
+COMMON_OPTS_COSPLAY_SATSUEI=('--modify-window=1' '--exclude=._*' '--exclude=.fseventsd' '--exclude=.Spotlight-V100' '--exclude=.Trashes' '--exclude=.DS_Store' '--exclude=*~' '--iconv=utf-8-mac,utf-8')
 
 case $value in
 	1)
 		rsync -avhWPn "${COMMON_OPTS[@]}" ~/Downloads/EOSR6M2/ 192.168.0.100:"/mnt/CosGallery/EOSR6M2/"
 		rsync -avhWPn "${COMMON_OPTS[@]}" ~/Downloads/done 192.168.0.100:"/mnt/CosGallery/"
-		rsync -avhWPn "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" 192.168.0.100:"/mnt/CosGallery/"
+		rsync -avhWPn "${COMMON_OPTS_COSPLAY_SATSUEI[@]}" "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" 192.168.0.100:"/mnt/CosGallery/"
 		rsync -avhWPn "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Pictures/Lightroom Projects" 192.168.0.100:"/mnt/CosGallery/"
 		;;
 	2)
 		rsync -avhWP "${COMMON_OPTS[@]}" ~/Downloads/EOSR6M2/ 192.168.0.100:"/mnt/CosGallery/EOSR6M2/"
 		rsync -avhWP "${COMMON_OPTS[@]}" ~/Downloads/done 192.168.0.100:"/mnt/CosGallery/"
-		rsync -avhWP "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" 192.168.0.100:"/mnt/CosGallery/"
+		rsync -avhWP "${COMMON_OPTS_COSPLAY_SATSUEI[@]}" "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" 192.168.0.100:"/mnt/CosGallery/"
 		rsync -avhWP "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Pictures/Lightroom Projects" 192.168.0.100:"/mnt/CosGallery/"
 		;;
 	*)
