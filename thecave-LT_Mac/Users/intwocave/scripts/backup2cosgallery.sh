0f4152779ceb3ceb2fb3cba895372a8eb4e58528
intwocave <intwocave@gmail.com>
2026-09-15 13:36:01 +0900

    feat: update backup2cosgallery.sh

--- thecave-LT_Mac/Users/intwocave/scripts/backup2cosgallery.sh
+++ thecave-LT_Mac/Users/intwocave/scripts/backup2cosgallery.sh
@@ -5,18 +5,20 @@ echo "2) Real"
 echo -n "-> "
 read value
 
+COMMON_OPTS=('--modify-window=1' '--exclude=._*' '--exclude=.fseventsd' '--exclude=.Spotlight-V100' '--exclude=.Trashes' '--exclude=.DS_Store' '--exclude=*~' '--delete' '--delete-excluded' '--iconv=utf-8-mac,utf-8')
+
 case $value in
 	1)
-		rsync -avhWPn --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete ~/Downloads/EOSR6M2/ "/Volumes/CosGallery/EOSR6M2/"
-		rsync -avhWPn --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete ~/Downloads/done "/Volumes/CosGallery/"
-		rsync -avhWPn --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" "/Volumes/CosGallery/"
-		rsync -avhWPn --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete "/Volumes/Clow - Data/Users/monakio/Pictures/Lightroom Projects" "/Volumes/CosGallery/"
+		rsync -avhWPn "${COMMON_OPTS[@]}" ~/Downloads/EOSR6M2/ 192.168.0.100:"/mnt/CosGallery/EOSR6M2/"
+		rsync -avhWPn "${COMMON_OPTS[@]}" ~/Downloads/done 192.168.0.100:"/mnt/CosGallery/"
+		rsync -avhWPn "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" 192.168.0.100:"/mnt/CosGallery/"
+		rsync -avhWPn "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Pictures/Lightroom Projects" 192.168.0.100:"/mnt/CosGallery/"
 		;;
 	2)
-		rsync -avhWP --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete ~/Downloads/EOSR6M2/ "/Volumes/CosGallery/EOSR6M2/"
-		rsync -avhWP --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete ~/Downloads/done "/Volumes/CosGallery/"
-		rsync -avhWP --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" "/Volumes/CosGallery/"
-		rsync -avhWP --exclude="._*" --exclude=".fseventsd" --exclude=".Spotlight-V100" --exclude=".Trashes" --exclude=".DS_Store" --exclude="*~" --delete "/Volumes/Clow - Data/Users/monakio/Pictures/Lightroom Projects" "/Volumes/CosGallery/"
+		rsync -avhWP "${COMMON_OPTS[@]}" ~/Downloads/EOSR6M2/ 192.168.0.100:"/mnt/CosGallery/EOSR6M2/"
+		rsync -avhWP "${COMMON_OPTS[@]}" ~/Downloads/done 192.168.0.100:"/mnt/CosGallery/"
+		rsync -avhWP "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Downloads/코스프레촬영" 192.168.0.100:"/mnt/CosGallery/"
+		rsync -avhWP "${COMMON_OPTS[@]}" "/Volumes/Clow - Data/Users/monakio/Pictures/Lightroom Projects" 192.168.0.100:"/mnt/CosGallery/"
 		;;
 	*)
 		echo "Invalid value"
